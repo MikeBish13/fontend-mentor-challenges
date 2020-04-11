792b89f06923d7f2d1c09280d16d641933c1d086
Michael Bishop <michael.bishop13@gmail.com>
2020-04-11 11:10:23 +0100

    Add files via upload

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -1,6 +1,5 @@
 window.onload=function(){
 
-<<<<<<< HEAD
 
 const form = document.getElementById("form");
 const email = document.querySelector(".email")
@@ -21,27 +20,6 @@ function validateEmail(email) {
   return re.test(String(email).toLowerCase());
 }
 
-=======
-const email = document.querySelector(".email");
-const errorMessage = document.querySelector(".error-message");
-
-email.addEventListener("input", function (event) {
-    if(email.validity.valueMissing) {
-        // If the field is empty
-        // display the following error message.
-        errorMessage.textContent = 'Please provide a valid email address.';
-      } else if(email.validity.typeMismatch) {
-        // If the field doesn't contain an email address
-        // display the following error message.
-        errorMessage.textContent = 'Please provide a valid email address.';
-      } else if(email.validity.tooShort) {
-        // If the data is too short
-        // display the following error message.
-        errorMessage.textContent = `Email should be at least ${ email.minLength } characters; you entered ${ email.value.length }.`;
-      }
-});
-
->>>>>>> 42fc362b4523c667821cb7a6caf77c2bae2513cd
 
 
 }
