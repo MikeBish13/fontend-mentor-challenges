8ca900d55d8b1964bfaf3cfa27562cf2bc5e6894
Michael Bishop <michael.bishop13@gmail.com>
2020-04-11 10:45:10 +0100

    Add files via upload

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -1,5 +1,27 @@
 window.onload=function(){
 
+<<<<<<< HEAD
+
+const form = document.getElementById("form");
+const email = document.querySelector(".email")
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let emailVal = email.value;
+    if(!validateEmail(emailVal)) {
+        form.classList.add("error");
+      } else {
+        form.classList.remove("error");
+      }
+
+});
+
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+=======
 const email = document.querySelector(".email");
 const errorMessage = document.querySelector(".error-message");
 
@@ -19,6 +41,7 @@ email.addEventListener("input", function (event) {
       }
 });
 
+>>>>>>> 42fc362b4523c667821cb7a6caf77c2bae2513cd
 
 
 }
